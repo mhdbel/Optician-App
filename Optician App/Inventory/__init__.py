abeb7c8d50787a751fa3a477f5a7d0d987c915d7
mhdbel <155534967+mhdbel@users.noreply.github.com>
2025-04-29 22:51:26 +0100

    Delete Optician App/Inventory directory

--- Optician App/Inventory/__init__.py
+++
@@ -1,7 +0,0 @@
-# inventory/__init__.py
-# This file can be empty, or you could import key elements:
-from .models import Item
-from .services import get_all_items, create_item
-
-# Now other modules can import directly from the inventory package:
-from inventory import Item, create_item
